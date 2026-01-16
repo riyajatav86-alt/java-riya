639dc120243bb1a8737e663f83dfd981507ace5a
Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>
2026-01-16 22:50:35 +0530

    Add files via upload
    Signed-off-by: Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>

---
+++ classs.java
@@ -0,0 +1,7 @@
+public class classs {
+    
+    public static void main(String[] args) {
+        System.out.println("This is the classs class.");
+    }
+    
+}
---
+++ jatav.java
@@ -0,0 +1,8 @@
+public class jatav {
+    public static void main(String[] args) {
+        System.out.println("This is the jatav class.");
+    }
+
+
+    
+}
